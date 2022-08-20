c7bd76e227f16310b56c9037e9db38bccb40ee5f
Pavannayak39 <110302752+Pavannayak39@users.noreply.github.com>
2022-08-20 20:35:05 +0530

    Add files via upload

---
+++ calclator.py
@@ -0,0 +1,18 @@
+a=float(input('enther your own value '))
+b=float(input('enter  your own value '))
+
+x=input("enter the xator ")
+
+if x=='+':
+    print(a+b)
+elif x=='-':
+    print(a-b)
+elif x=='*':
+    print(a*b)
+elif x=='/':
+    print(a/b)
+elif x=='**':
+    print(a**b)
+else:
+    print(a//b)    
+
